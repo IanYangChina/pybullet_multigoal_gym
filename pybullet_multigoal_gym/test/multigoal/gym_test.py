3d1ec527d7fe414545b87846c102341398db9677
Xintong <xintongyang1994@foxmail.com>
2021-08-26 09:56:44 +0100

    pick and place task ee start above table

--- pybullet_multigoal_gym/test/multigoal/gym_test.py
+++ pybullet_multigoal_gym/test/multigoal/gym_test.py
@@ -4,7 +4,7 @@ import time
 import pybullet_multigoal_gym as pmg
 
 num_episodes = 300
-env = pmg.make_env(task='block_stack',
+env = pmg.make_env(task='chest_pick_and_place',
                    gripper='parallel_jaw',
                    num_block=2,
                    render=True,
